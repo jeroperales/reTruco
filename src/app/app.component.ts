18f3688dc0aeeb782e79f24bae0390a1621add78
jeroperales <jeronimoperales1@gmail.com>
2025-01-13 13:03:27 -0300

    modified home, added grid and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PointsComponent } from "./components/points/points.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PointsComponent } from './components/points/points.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+
+    {
+        path: 'anotador',
+        component: PointsComponent
+    },
+    {
+        path: '',
+        redirectTo:'home',
+        pathMatch: 'full'
+    }
+];
